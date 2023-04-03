2a91d0810aa4cb630d028d18584e0433d4bfbfe9
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-04-03 19:35:32 -0400

    added 'drop income from base_debiased' accuracy test

---
+++ b_d_income_bins.py
@@ -0,0 +1,25 @@
+import classifier
+
+DATASET_PATH = "datasets/base_debiased.csv"
+
+onehot_cols = [
+    "Married/Single",
+    "House_Ownership",
+    "Profession",
+    "CITY",
+    "STATE"
+]
+
+enum_cols = [
+    "Id"
+]
+
+yesno_cols = [
+    "Car_Ownership"
+]
+
+result           = classifier.run(DATASET_PATH, onehot_cols, enum_cols, yesno_cols, "Risk_Flag", should_print=True)
+result_no_income = classifier.run(DATASET_PATH, onehot_cols, enum_cols, yesno_cols, "Risk_Flag", drop_cols = ["Income"], should_print=False)
+print("CLASSIFICATION RESULTS:")
+print("accuracy:", result["accuracy"])
+print("accuracy without:", result_no_income["accuracy"])
